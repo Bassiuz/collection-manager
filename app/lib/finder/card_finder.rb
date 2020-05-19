872229307c001e907220b1be762d4f9b38b7af44
Bas de Vaan <bas.de.vaan@kabisa.nl>
2020-05-19 22:24:27 +0200

    add more cardfinder test

--- app/lib/finder/card_finder.rb
+++ app/lib/finder/card_finder.rb
@@ -6,8 +6,9 @@ module Finder
   class CardFinder
     attr_accessor :break_up_decks
 
-    def initialize(break_up_decks)
-      @break_up_decks = break_up_decks
+    def initialize(params)
+      @break_up_decks = params[:break_up_decks]
+      
     end
 
     def find_cards_for_name(card_names)
@@ -24,17 +25,30 @@ module Finder
     def find_card_for_name(card_row)
       amount, name = card_row
       amount = amount.gsub('x', '').to_i
+
+      if @break_up_decks
+        @available_boxes = Box.all
+      else
+        @available_boxes = Box.where.not(leave_box_in_tact: true)
+      end
+
       amount.times do
+        matching_cards = Card.where(name: name, box: @available_boxes)
+        return if matching_cards.empty?
+        
         if @cards.empty?
-          @cards << Card.where(name: name).first
+          @cards << matching_cards.first
         else
-          matching_cards = Card.where(name: name)
-          byebug
-          unless matching_cards.empty?
-            @cards << matching_cards.where(id: @cards.map(&:id)).first
-          end
+          find_card_not_already_found(matching_cards)
         end
       end
+    end
+
+    def find_card_not_already_found(matching_cards)
+      return if matching_cards.empty?
+
+      found_card = matching_cards.where.not(id: @cards.map(&:id)).first
+      @cards << found_card unless found_card.nil?
     end
   end
 end
--- spec/lib/finder/card_finder_spec.rb
+++ spec/lib/finder/card_finder_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Finder::CardFinder do
         @cards = described_class.new(break_up_decks: true).find_cards_for_name(input)
       end
 
-      let(:input) { '1x; Vastwood Hydra; 1x; Tamiyo, Collector of Tales;' }
+      let(:input) { '1x; Vastwood Hydra; 1x; Tamiyo, Collector of Tales' }
 
       before do
         @cards = []
@@ -20,7 +20,7 @@ RSpec.describe Finder::CardFinder do
         box.cards.create!(name: 'Tamiyo, Collector of Tales', set: 'War of the Spark')
         box.cards.create!(name: 'Blade of the Bloodchief', set: 'Zendikar')
 
-        # InputParser.parse_input(:input)
+        # InputParser.parse_input(:input) Patrick: how do i load this class? (if I wanted to?)
       end
 
       it 'returns the amount of cards given in the input' do
@@ -33,21 +33,115 @@ RSpec.describe Finder::CardFinder do
           @cards = described_class.new(break_up_decks: true).find_cards_for_name(input)
         end
   
-        let(:input) { '1x; Vastwood Hydra; 3x; Tamiyo, Collector of Tales;' }
+        let(:input) { '1x; Vastwood Hydra; 3x; Tamiyo, Collector of Tales' }
   
         before do
           @cards = []
           box = Box.create!(name: 'test', size: 200)
-          box.cards.create!(name: 'Vastwood Hydra', set: 'Magic 2014')
-          box.cards.create!(name: 'Tamiyo, Collector of Tales', set: 'War of the Spark')
-          box.cards.create!(name: 'Tamiyo, Collector of Tales', set: 'War of the Spark')
+          @expected_results = []
+          @expected_results << box.cards.create!(name: 'Vastwood Hydra', set: 'Magic 2014')
+          @expected_results << box.cards.create!(name: 'Tamiyo, Collector of Tales', set: 'War of the Spark')
+          @expected_results << box.cards.create!(name: 'Tamiyo, Collector of Tales', set: 'War of the Spark')
+          pp @expected_results
           box.cards.create!(name: 'Blade of the Bloodchief', set: 'Zendikar')
   
           # InputParser.parse_input(:input)
         end
   
         it 'returns the amount of cards available, not the amount requested' do
-          expect { subject }.to change { @cards.count }.by 2
+          expect { subject }.to change { @cards.count }.by 3
+        end
+
+        it 'contains the expected cards' do
+          expect { subject }
+            .to change { @cards }
+            .to contain_exactly(@expected_results[0], @expected_results[1], @expected_results[2])
+            #.to contain_exactly(@expected_results) Patrick: doesn't work? 
+        end
+      end
+
+      describe 'With a list of cards that is divided between boxes' do
+        subject do
+          @cards = described_class.new(break_up_decks: true).find_cards_for_name(input)
+        end
+  
+        let(:input) { '1x; Vastwood Hydra; 2x; Tamiyo, Collector of Tales' }
+  
+        before do
+          @cards = []
+          box = Box.create!(name: 'test', size: 200, leave_box_in_tact: true)
+          @expected_results = []
+          @expected_results << box.cards.create!(name: 'Vastwood Hydra', set: 'Magic 2014')
+          @expected_results << box.cards.create!(name: 'Tamiyo, Collector of Tales', set: 'War of the Spark')
+
+          box2 = Box.create!(name: 'test2', size: 200)
+          box2.cards.create!(name: 'Vastwood Hydra', set: 'Magic 2014')
+          @expected_results << box2.cards.create!(name: 'Tamiyo, Collector of Tales', set: 'War of the Spark')
+          box2.cards.create!(name: 'Blade of the Bloodchief', set: 'Zendikar')
+  
+          # InputParser.parse_input(:input)
+        end
+  
+        it 'returns the amount of cards available]' do
+          expect { subject }.to change { @cards.count }.by 3
+        end
+
+        it 'contains the expected cards' do
+          expect { subject }
+            .to change { @cards }
+            .to contain_exactly(@expected_results[0], @expected_results[1], @expected_results[2])
+            #.to contain_exactly(@expected_results) Patrick: doesn't work? 
+        end
+      end
+
+      describe 'With a list of cards that are in decks that cant be broken up' do
+        subject do
+          @cards = described_class.new(break_up_decks: false).find_cards_for_name(input)
+        end
+  
+        let(:input) { '1x; Vastwood Hydra; 2x; Tamiyo, Collector of Tales' }
+  
+        before do
+          @cards = []
+          box = Box.create!(name: 'test', size: 200, leave_box_in_tact: true)
+          @expected_results = []
+          box.cards.create!(name: 'Vastwood Hydra', set: 'Magic 2014')
+          box.cards.create!(name: 'Tamiyo, Collector of Tales', set: 'War of the Spark')
+
+          box2 = Box.create!(name: 'test2', size: 200, leave_box_in_tact: false)
+          @expected_results << box2.cards.create!(name: 'Tamiyo, Collector of Tales', set: 'War of the Spark')
+          box2.cards.create!(name: 'Blade of the Bloodchief', set: 'Zendikar')
+  
+          # InputParser.parse_input(:input)
+        end
+  
+        it 'returns the amount of cards available in usable boxes' do
+          expect { subject }.to change { @cards.count }.by 1
+        end
+
+        it 'contains the expected cards' do
+          expect { subject }
+            .to change { @cards }
+            .to contain_exactly(@expected_results[0])
+            #.to contain_exactly(@expected_results) Patrick: doesn't work? 
+        end
+      end
+
+      describe 'With a list of card that have no matches' do
+        subject do
+          @cards = described_class.new(break_up_decks: false).find_cards_for_name(input)
+        end
+  
+        let(:input) { '1x; Vastwood Hydra; 2x; Tamiyo, Collector of Tales' }
+  
+        before do
+          @cards = []
+          box = Box.create!(name: 'test', size: 200, leave_box_in_tact: true)
+          box.cards.create!(name: 'Blade of the Bloodchief', set: 'Zendikar')
+        end
+  
+        it 'to return nothing' do
+          expect { subject }.to_not change{@cards}
         end
       end
   end
